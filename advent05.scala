3808434778cf2234302416080ddec84c230c02c8
Dmitry Smirnov <dmi.smirnov07@gmail.com>
2016-03-23 13:29:15 +0200

    Create advent05.scala

---
+++ advent05.scala
@@ -0,0 +1,25 @@
+import scala.util.matching.Regex
+
+val input = scala.io.Source.fromFile("advent05.txt").mkString.split("\n")
+
+// Using regex
+val patternVowels = new Regex("[aeiou]")
+val patternLetterTwice = new Regex("([a-z])\\1+")
+val patternExceptions = new Regex("(ab|cd|pq|xy)")
+
+def isNotNaughty(s: String): Boolean = {
+  val t1 = patternVowels.findAllIn(s).size >= 3
+  val t2 = patternLetterTwice.findFirstIn(s).size >= 1
+  val t3 = patternExceptions.findFirstIn(s).size == 0
+  t1 & t2 & t3  
+}
+
+val r1 = input.filter(isNotNaughty(_)).size
+
+
+/* some wasted effort, disregard
+def containsNumVowels(s: String, num: Int): Boolean = {
+  require (num > 0, "Num should be larger than zero, now: " + num)  
+  val vowels = "aeiou"
+  s.filter(vowels.indexOf(_) >= 0).size >= num
+}*/

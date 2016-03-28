a027e8288aa9ed06b25e51397ddad9577e29d179
Dmitry Smirnov <dmi.smirnov07@gmail.com>
2016-03-28 13:37:38 +0300

    Update advent05.scala

--- advent05.scala
+++ advent05.scala
@@ -16,6 +16,23 @@ def isNotNaughty(s: String): Boolean = {
 
 val r1 = input.filter(isNotNaughty(_)).size
 
+// For second star
+def hasNonoverlappingPair(line: String): Boolean = {
+// If line has pairs that are distanced by more than 1 character
+  line.sliding(2).toList.exists{ c =>
+    line.lastIndexOf(c) - line.indexOf(c) > 1
+  }
+}
+
+def sandwiched(line: String): Boolean = {
+  line.sliding(3).toList.exists{ t =>
+    t(0) == t(2)
+  }
+}
+
+val r2 = input.filter { line => 
+  sandwiched(line) & hasNonoverlappingPair(line)
+}.length
 
 /* some wasted effort, disregard
 def containsNumVowels(s: String, num: Int): Boolean = {

5526b0c9e5af180ce0cd42ba189c91aa3d6e970b
Dmitry Smirnov <dmi.smirnov07@gmail.com>
2016-04-27 10:59:04 +0300

    Update advent08.scala

--- advent08.scala
+++ advent08.scala
@@ -1,4 +1,8 @@
 
+def addEnclosingQuotes(line: String): String = {
+  "\"" + line + "\""
+}
+
 def dropEnclosingQuotes(line: String): String = {
   line.drop(1).dropRight(1)
 }
@@ -10,7 +14,6 @@ implicit class TupleAdd[A : Numeric, B : Numeric](t: (A, B)) {
 }
 
 def scanString(line: String): (Int, Int) = {
-
   // we move one char at a time and inspect if it is backlash
   var current = 0
   var codeRep = 2 // start at 2 because we already accounted for enclosing quotes
@@ -44,9 +47,37 @@ def scanString(line: String): (Int, Int) = {
   (codeRep, stringRep)
 }
 
+def convertString(line: String): String = {
+  /*// Procedural style
+  var current = 0
+  var stack = ""
+  while (current < line.length) {
+    line(current).toString match {
+      case "\"" => stack += """\""""
+      case "\\" => stack += """\\"""
+      case c => stack += c
+    }
+    current += 1
+  }
+  */
+  // functional style
+  line.split("").map {
+    case "\"" => """\""""
+    case "\\" => """\\"""
+    case c => c
+  }.mkString
+}
+
 val input = scala.io.Source.fromFile("/m/nbe/home/smirnod1/advent08.txt").mkString.split("\n")
 val noEnclosingQuotes = input.map(dropEnclosingQuotes)
 val result = noEnclosingQuotes.map(scanString).foldLeft((0,0))(_+_)
 var r1 = result._1 - result._2
+
+val newInput = input.map(convertString)
+val result = newInput.map(scanString).foldLeft((0,0))(_+_)
+var r2 = result._1 - result._2
+
+
+
 
 
